dc2f246181df6390d1d71f4f6a3bcdc4a75776f3
shinya1024 <65591114+shinya1024@users.noreply.github.com>
2020-05-28 16:49:04 +0900

    微分方程式の練習２

---
+++ bibunn3.py
@@ -0,0 +1,30 @@
+
+
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+
+def model(x,t,a):
+    dxdt = -a * x
+    return dxdt
+
+x0 = 5
+
+t = np.linspace(0,100)
+
+a = 0.1
+x1 = odeint(model,x0,t,args=(a,))
+a = 0.2
+x2 = odeint(model,x0,t,args=(a,))
+a = 0.5
+x3 = odeint(model,x0,t,args=(a,))
+
+plt.plot(t,x1,'r-',linewidth = 2,label = 'a = 0.1')
+plt.plot(t,x2,'b--',linewidth = 2,label = 'a = 0.2')
+plt.plot(t,x3,'g:',linewidth = 2,label = 'a = 0.5')
+
+plt.xlabel('time')
+plt.ylabel('x(t)')
+plt.legend()
+plt.show()
