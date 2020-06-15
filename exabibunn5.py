499e608fe3ee89cc7ae5ecd83dbc2089780a8acd
shinya1024 <65591114+shinya1024@users.noreply.github.com>
2020-06-15 17:18:58 +0900

    連立微分

--- exabibunn5.py
+++ exabibunn5.py
@@ -5,21 +5,23 @@ import matplotlib.pyplot as plt
 def bibunn(t,z,a,b,c,d):
 	 x, y = z
 	 return [-a*x +b*y, c*x - d*y]
+t_min = 0
+t_max = 15
+xinit = 0.4
+yinit = 0.6
+sol = solve_ivp(bibunn, [t_min,t_max], [xinit, yinit], args=(1, 2, 1, 2),t_eval = np.linspace(t_min,t_max,1000))
 
-sol = solve_ivp(bibunn, [0,15], [0.4, 0.6], args=(2000, 1000, 2000, 1000),
-                 dense_output=True)
-
-t = np.linspace(0,15,3)
 
 
-z = sol.sol(t)
 
-plt.plot(t,z.T)
+
+
+plt.plot(sol.t,sol.y.T)
 plt.xlabel('t')
 plt.legend(['x','y'],shadow = True)
 plt.title('System')
-plt.xlim([0,15])
+plt.xlim([0,2])
 plt.ylim([0,1])
-#plt.show()
+plt.show()
 
-plt.savefig('bibunn2.png')
+#plt.savefig('bibunn2.png')
