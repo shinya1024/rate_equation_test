21d0c5cd7e64e984e3675d9946d87092c425c652
shinya1024 <65591114+shinya1024@users.noreply.github.com>
2020-07-10 21:52:34 +0900

    HeH+

--- rate.py
+++ rate.py
@@ -1,18 +1,20 @@
 from scipy.integrate import solve_ivp
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 def rate(t,v,k1,k2,k3):
 	 w, x, y, z = v
-	 return [k2*y - (k1 + k3)*w, k2*y + k3*(w + y),k1*z*w - (k2 + k3)*y,-k1*z]
+	 return [k2*y - (k1 + k3)*w, k2*y + k3*(w + y),k1*z*w - (k2 + k3)*y,- k1 * z]
+	 
 t_min = 0
-t_max = 15000000000000000
+t_max = 1.5*10e23
 winit = 0.4
 xinit = 0.2
 yinit = 0.3
 zinit = 0.1
-sol = solve_ivp(rate, [t_min,t_max], [winit, xinit, yinit, zinit], args=(0.000000000000000000000014,0.0000000000000003,0.0000000000000012),t_eval = np.linspace(t_min,t_max,1000))
 
+sol = solve_ivp(rate, [t_min,t_max], [winit, xinit, yinit, zinit], args=(1.4*10**(-22),1.6*10**(-15),1.8*10**(-15)),t_eval = np.linspace(t_min,t_max,10000),method = "Radau")
 
 
 
@@ -21,6 +23,8 @@ plt.plot(sol.t,sol.y.T)
 plt.xlabel('t')
 plt.legend(['w','x','y','z'],shadow = True)
 plt.title('System')
-#plt.xlim([0,0.8])
+plt.xscale('log')
+#plt.yscale('log')
+plt.xlim([10e12,10e16])
 #plt.ylim([0,1])
 plt.show()
