b3f142a8aa6840909767d359b0e82b7b97928ba5
shinya1024 <65591114+shinya1024@users.noreply.github.com>
2020-07-11 12:54:49 +0900

    HeH+

--- rate.py
+++ rate.py
@@ -7,11 +7,11 @@ def rate(t,v,k1,k2,k3):
 	 return [k2*y - (k1 + k3)*w, k2*y + k3*(w + y),k1*z*w - (k2 + k3)*y,- k1 * z]
 	 
 t_min = 0
-t_max = 1.5*10e23
-winit = 0.4
+t_max = 1.0*10e23
+winit = 0.1
 xinit = 0.2
 yinit = 0.3
-zinit = 0.1
+zinit = 0.4
 
 sol = solve_ivp(rate, [t_min,t_max], [winit, xinit, yinit, zinit], args=(1.4*10**(-22),1.6*10**(-15),1.8*10**(-15)),t_eval = np.linspace(t_min,t_max,10000),method = "Radau")
 
@@ -24,6 +24,6 @@ plt.legend(['w','x','y','z'],shadow = True)
 plt.title('System')
 plt.xscale('log')
 #plt.yscale('log')
-#plt.xlim([10e12,10e16])
-#plt.ylim([0,1])
+plt.xlim([10e12,10e23])
+#plt.ylim([0,0.3])
 plt.show()
